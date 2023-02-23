4ea94cedacdaff678bbd0e955a3680844af81ea3
tandrewton-desktop <andrew.ton@yale.edu>
2023-02-23 17:03:59 -0500

    improving job file names for dsq_scripts

--- dsq_scripts_bank.sh
+++ dsq_scripts_bank.sh
@@ -14,14 +14,14 @@ for t_stress in ${t_stress_arr[@]}; do
     for om in 0.01; do
       for kl in 1.0; do
         for ka in 1.0; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_tau_att.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_tau_att.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
 
 module load dSQ
@@ -40,14 +40,14 @@ for t_stress in ${t_stress_arr[@]}; do
     for om in ${om_arr[@]}; do
       for kl in 1.0; do
         for ka in 1.0; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_tau_om.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_tau_om.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
 
 module load dSQ
@@ -66,14 +66,14 @@ for t_stress in ${t_stress_arr[@]}; do
     for om in 0.01; do
       for kl in 1.0; do
         for ka in ${ka_arr[@]}; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_tau_ka.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_tau_ka.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
 
 module load dSQ
@@ -92,14 +92,14 @@ for t_stress in 100000.0; do
     for om in ${om_arr[@]}; do
       for kl in 1.0; do
         for ka in 1.0; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_att_om.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_att_om.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
 module load dSQ
 # testing stress relaxation, attraction, boundaries on
@@ -117,14 +117,14 @@ for t_stress in 100000.0; do
     for om in 0.01; do
       for kl in 1.0; do
         for ka in ${ka_arr[@]}; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_att_ka.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_att_ka.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
 module load dSQ
 # testing stress relaxation, attraction, boundaries on
@@ -142,14 +142,14 @@ for t_stress in 100000.0; do
     for om in ${om_arr[@]}; do
       for kl in 1.0; do
         for ka in ${ka_arr[@]}; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_om_ka.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_om_ka.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
 
 module load dSQ
@@ -168,14 +168,14 @@ for t_stress in 100000.0; do
     for om in 0.01; do
       for kl in ${kl_arr[@]}; do
         for ka in ${ka_arr[@]}; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_kl_ka.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_kl_ka.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
 # runs for presentation
 module load dSQ
@@ -194,12 +194,12 @@ for t_stress in 100000.0; do
     for om in ${om_arr[@]}; do
       for kl in 1.0; do
         for ka in 1.0; do
-            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS.txt
+            echo bash bash/epi2D/submit_laserAblation.sh 50 30 3 1.10 0.94 0.85 $kl $ka 0.01 $att $om 4.0 1.0 4.0 1.0 0.0 $t_stress 0 1 1000 pi_ohern,day,scavenge 0-24:00:00 $numSeeds 1 >> joblist_PS_att_om.txt
         done
       done
     done
   done
 done
 
-dsq --job-file joblist_PS.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
+dsq --job-file joblist_PS_att_om.txt --mem-per-cpu 4g -t 24:00:00 --mail-type NONE --submit --suppress-stats-file  -o /dev/null
 
