9a27e960c46d9afe22dd1d40c56aefa6840b67ab
tandrewton-desktop <andrew.ton@yale.edu>
2023-06-29 16:34:30 -0400

    phi array in runscript :

--- psm_scripts_bank.sh
+++ psm_scripts_bank.sh
@@ -1,19 +1,21 @@
-
 module load dSQ
 #!/bin/bash
 numSeeds=1
-calA0=(1.05)
-att_arr=(0 0.01 0.05 0.1)
-v0_arr=(0.05)
-kecm_arr=(0.1 1.0 10.0)
+calA0=(1.0)
+phi_arr=(0.74 0.63 0.53)
+att_arr=(0 0.001 0.01 0.1)
+v0_arr=(0.005)
+kecm_arr=(0.006 0.06 0.6)
 koff_arr=(1.0 10.0 100.0)
 rm joblist_psm_att_v0_koff.txt
-for att in ${att_arr[@]}; do 
-  for v0 in ${v0_arr[@]}; do
-    for koff in ${koff_arr[@]}; do
-        for kecm in ${kecm_arr[@]}; do
-            echo bash bash/cells/submit_psm.sh 40 20 $calA0 0.75 $att 0.0 $v0 50.0 $kecm $koff 1000 pi_ohern,day 0-4:00:00 $numSeeds 1 >> joblist_psm_att_v0_koff.txt
-        done
+for phi in ${phi_arr[@]}; do
+  for att in ${att_arr[@]}; do 
+    for v0 in ${v0_arr[@]}; do
+      for koff in ${koff_arr[@]}; do
+          for kecm in ${kecm_arr[@]}; do
+              echo bash bash/cells/submit_psm.sh 40 20 $calA0 $phi $att 0.0 $v0 1.0 $kecm $koff 400 pi_ohern,day 0-4:00:00 $numSeeds 1 >> joblist_psm_att_v0_koff.txt
+          done
+      done
     done
   done
 done
@@ -25,11 +27,11 @@ bash bash/cells/submit_psm.sh 40 20 1.05 0.9 0 0.0 0.05 50.0 1.0 10.0 1000 pi_oh
 rsync -rav --inplace --progress at965@transfer-grace.hpc.yale.edu:/gpfs/gibbs/pi/ohern/at965/dpm/psm /mnt/c/Users/atata/projects/dpm/pipeline/cells/. 
 
 close all; clear;
-calA0_arr = ["1.05"];
-att_arr = ["0" "0.01" "0.05" "0.1"];
+calA0_arr = ["1.0"];
+att_arr = ["0" "0.001" "0.01" "0.1"];
 phi_arr = ["0.74" "0.63" "0.53"]
-v0_arr = ["0.05"];
-k_ecm_arr = ["0.1" "1.0" "10.0"];
+v0_arr = ["0.005"];
+k_ecm_arr = ["0.006" "0.06" "0.6"];
 k_off_arr = ["1.0" "10.0" "100.0"];
 for ii=1:length(calA0_arr)
     for jj=1:length(phi_arr)
