0f447aa2ae94a238eff1371d3f165c4ee22824f8
Andrew Ton <atatat123@gmail.com>
2023-07-17 10:13:23 -0400

    unstashing local changes to msd

--- msd.m
+++ msd.m
@@ -1,6 +1,7 @@
-%% calculate msd and other dynamic quantities
+%% calculate msd 
 % will write for a specific file, then refactor into helper functions?
 close all; clf; clear
+set(0,'DefaultFigureWindowStyle','docked')
 filename = "test8.msd";
 data = load(filename);
 time = data(:,1);
@@ -18,10 +19,25 @@ for lag = 1:maxLag
     msd_per_particle_per_lag(:, lag) = mean(msd_per_particle, 1);
 end
 MSD = mean(msd_per_particle_per_lag, 1);
-
 figure()
 plot(time(2:maxLag), MSD(2:end),'k')
-%%
+
+%% plot cell tracks
+% divide cellPos (# timesteps x 2N) into xCoords and yCoords, which are (#
+% timesteps x N)
+xCoords = cellPos(:,1:2:end);
+yCoords = cellPos(:,2:2:end);
+
+% Plot cell tracks
+figure;
+hold on;
+for i = 1:size(xCoords, 2)
+    plot(xCoords(:, i), yCoords(:, i), '-','linewidth', 2);
+end
+hold off;
+axis equal off;
+
+%% plot neighbor exchange count vs time
 filename = "test8.cellContact";
 delimiter = ','; % Delimiter used within each matrix
 
@@ -29,8 +45,39 @@ delimiter = ','; % Delimiter used within each matrix
 fileContent = fileread(filename);
 
 % Split the string into individual matrices using newline separations
-matrices = strsplit(fileContent, '\n\n');
+matricesStr = strsplit(fileContent, '\n\n');
 
 % Remove empty cell from last newline character
-nonemptyIndices = ~cellfun(@isempty, matrices);
-matrices = matrices(nonemptyIndices);
+nonemptyIndices = ~cellfun(@isempty, matricesStr);
+matricesStr = matricesStr(nonemptyIndices);
+
+NE = zeros(1, size(matricesStr,2));
+
+% Process each matrix
+for i = 1:numel(matricesStr)
+    % Split the matrix string into rows
+    rowsStr = strsplit(matricesStr{i}, '\n');
+    rowsStr(cellfun('isempty', rowsStr)) = [];
+    
+    % Split rows into values
+    values = cellfun(@(x) strsplit(x, delimiter), rowsStr, 'UniformOutput', false);
+    
+    % Convert values to a numeric matrix
+    % matrix(i,j) = # vertex contacts between cell i and cell j at a
+    % particular time 
+    matrix = str2double(vertcat(values{:}));
+    matrix(matrix >= 1) = 1;
+
+    % move to next iteration if we're on first, since we want differences
+    % in time
+    if i == 1
+        continue;
+    end
+
+    % Process the matrix
+    newmat = matrix - oldmatrix;
+
+    % store matrix to compare next timestep
+    oldmatrix = matrix;
+end
+%%
