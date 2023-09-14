5b09f72b769f75881aab7b36d5d9ffde2c304e24
tandrewton-desktop <andrew.ton@yale.edu>
2023-09-14 14:05:54 -0400

    reorganizing analysis script

--- cellShapeAnalysis.py
+++ cellShapeAnalysis.py
@@ -6,7 +6,10 @@ import debugpy
 import csv
 from io import StringIO
 
-
+att_arr = ["0.001", "0.01", "0.1"]
+v0_arr = ["0.02"]
+k_ecm_arr = ["0.05", "0.5", "5"]
+seeds = 10
 
 def readCSV(filename):
     # currently omits contacts with the boundary
@@ -37,64 +40,73 @@ def calculateNeighborExchanges(contactMatrix):
         nonzeros = np.nonzero(contactMatrix[i+1] - contactMatrix[i])
     return diff
 
+def process_data(att, v0, k_ecm, seed, fileheader):
+    # fileheader = "test6"
+    outputFileheader = "output" + fileheader[8:]
+    fileExtensions = [".xStream", ".xMinStream",
+                      ".cijStream", ".cijMinStream", ".comStream", ".shapeStream"]
+    filenames = [fileheader + ext for ext in fileExtensions]
+    # coordinates of all vertices
+    cellPos = np.loadtxt(filenames[0])
+    # energy minimized coordinates
+    cellMinPos = np.loadtxt(filenames[1])
+    cij = readCSV(filenames[2])  # cell-cell contact network
+    # contact of minimized coordinates
+    cijMin = readCSV(filenames[3])
+    cellCOM = np.loadtxt(filenames[4])
+    cellShape = np.loadtxt(filenames[5])
+
+    # extract header information from cellPos and then remove header
+    NCELLS = int(cellPos[0][0])
+    NVTOT = int(cellPos[0][1])
+    cellPos = cellPos[1:]
+    globalXLim = [min(cellPos[:, 0]), max(cellPos[:, 0])]
+    globalYLim = [min(cellPos[:, 1]), max(cellPos[:, 1])]
+    shapeParameters = np.ravel(cellShape[:, 0::2])
+
+    data_shape = {
+        'shapeParameter': shapeParameters,
+        'att': float(att),
+        'v0': float(v0),
+        'k_ecm': float(k_ecm),
+        'seed': seed
+    }
+    df_shape = pd.DataFrame(data_shape)
+
+    minNE = calculateNeighborExchanges(cijMin)
+    data_NE = {
+        'minNE': minNE,
+        'att': float(att),
+        'v0': float(v0),
+        'k_ecm': float(k_ecm),
+        'seed': seed
+    }
+    df_NE = pd.DataFrame(data_NE)
+
+    return df_shape, df_NE
+
 
 def main():
-    att_arr = ["0.001", "0.01", "0.1"]
-    #att_arr = ["0.001", "0.01"]
-    v0_arr = ["0.02"]
-    #v0_arr = ["0.02", "0.04"]
-    #k_ecm_arr = ["0.05", "0.5"]
-    k_ecm_arr = ["0.05", "0.5", "5"]
-    seeds = 10
     df_shapes = pd.DataFrame()
     df_NEs = pd.DataFrame()
     for att in att_arr:
         for v0 in v0_arr:
             for k_ecm in k_ecm_arr:
                 for seed in range(1, seeds+1):
-                    # fileheader = "test6"
-                    fileheader = "pipeline\cells\psm\psm_calA01.0_phi0.74_tm10.0_v0"+v0+"_t_abp1.0k_ecm"+k_ecm+"k_off1.0\_N40_dur250_att"+att+"_start1_end"+str(seeds)+"_sd"+str(seed)
-                    outputFileheader = "output"+fileheader[8:]
-                    fileExtensions = [".xStream", ".xMinStream",
-                                    ".cijStream", ".cijMinStream", ".comStream", ".shapeStream"]
-                    filenames = [fileheader + ext for ext in fileExtensions]
-                    cellPos = np.loadtxt(filenames[0])  # coordinates of all vertices
-                    cellMinPos = np.loadtxt(filenames[1])  # energy minimized coordinates
-                    cij = readCSV(filenames[2])  # cell-cell contact network
-                    # cell-cell contact network of minimized coordinates
-                    cijMin = readCSV(filenames[3])
-                    cellCOM = np.loadtxt(filenames[4])
-                    cellShape = np.loadtxt(filenames[5])
-
-                    # extract header information from cellPos and then remove header
-                    NCELLS = int(cellPos[0][0])
-                    NVTOT = int(cellPos[0][1])
-                    cellPos = cellPos[1:]
-                    globalXLim = [min(cellPos[:, 0]), max(cellPos[:, 0])]
-                    globalYLim = [min(cellPos[:, 1]), max(cellPos[:, 1])]
-                    shapeParameters = np.ravel(cellShape[:, 0::2])
-                    df_shape = pd.DataFrame(shapeParameters, columns = ['shapeParameter'])
-                    df_shape['att'] = float(att)
-                    df_shape['v0'] = float(v0)
-                    df_shape['k_ecm'] = float(k_ecm)
-                    df_shape['seed'] = seed
+                    fileheader = f"pipeline\\cells\\psm\\psm_calA01.0_phi0.74_tm10.0_v0{v0}_t_abp1.0k_ecm{k_ecm}k_off1.0\\_N40_dur250_att{att}_start1_end{seeds}_sd{seed}"
+                    df_shape, df_NE = process_data(att, v0, k_ecm, seed, fileheader)
                     df_shapes = pd.concat([df_shapes, df_shape])
-
-                    minNE = calculateNeighborExchanges(cijMin)
-                    df_NE = pd.DataFrame(minNE, columns = ['minNE'])
-                    df_NE['att'] = float(att)
-                    df_NE['v0'] = float(v0)
-                    df_NE['k_ecm'] = float(k_ecm)
-                    df_NE['seed'] = seed
                     df_NEs = pd.concat([df_NEs, df_NE])
 
-    # add a column representing unique combinations of parameters. 
-    #   use lambda here to convert combo column values into strings,
-    #       then join the strings into one string so it can be read through seaborn
-    df_NEs["(att, k_ecm, v0)"] = df_NEs.apply(lambda row: ', '.join(
-        [str(row['att']), str(row['k_ecm']), str(row['v0'])]), axis=1)
-    
+    # Perform grouping on dataframes in order to make summary plots.
+
+    # Add a column representing unique combinations of parameters. 
+    # Use lambda here to convert combo column values into strings,
+    #  then join the strings into one string so it can be read through seaborn
     # group the dataframe by combo parameters, then average over matching seeds and return a dataframe
+
+    df_NEs["(att, k_ecm, v0)"] = df_NEs.apply(lambda row: ', '.join(
+        [str(row['att']), str(row['k_ecm']), str(row['v0'])]), axis=1)    
     df_NEs_means = df_NEs.groupby(["(att, k_ecm, v0)", "seed"]).mean().reset_index()
 
     df_shapes["(att, k_ecm, v0)"] = df_shapes.apply(lambda row: ', '.join(
@@ -102,14 +114,20 @@ def main():
     df_shapes_means = df_shapes.groupby(
         ["(att, k_ecm, v0)", "seed"]).mean().reset_index()
 
-    plt.figure()
-    sns.catplot(df_NEs_means, x="(att, k_ecm, v0)", y="minNE", kind="box", color="skyblue")
-
-    plt.figure()
+    # make boxplots of the mean values for each seed for each parameter combo
+    sns.catplot(df_NEs_means, x="(att, k_ecm, v0)", y="minNE", kind="box", color="skyblue", height = 8, aspect=2)
     sns.catplot(df_shapes_means, x="(att, k_ecm, v0)",
-                y="shapeParameter", kind="box", color="skyblue")
+                y="shapeParameter", kind="box", color="skyblue", height=8, aspect=2)
+    
+    # make boxplots for each parameter combo, pooling observations from each seed
+    sns.catplot(df_NEs, x="(att, k_ecm, v0)",
+                y="minNE", kind="box", color="skyblue", height=8, aspect=2)
+    sns.catplot(df_shapes, x="(att, k_ecm, v0)",
+                y="shapeParameter", kind="box", color="skyblue", height=8, aspect=2)
 
+    debugpy.breakpoint()
 
+    plt.show()
 
 
 if __name__ == "__main__":
